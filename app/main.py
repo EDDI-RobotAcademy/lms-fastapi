48e399f21ab55c26e019e833bacb20e58ecc6acc
Meue-L <meue7785@gmail.com>
2024-08-16 11:14:33 +0900

    feat: [MinJaeLee/Meue] fastapi에 소켓통신용 server 올리기 [LMSF-2]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,42 @@
+import os.path
+import sys
+
+import colorama
+import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from template.task_manager.manager import TaskManager
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template', 'include', 'socket_server'))
+
+from template.deep_learning.controller.deep_learning_controller import deepLearningRouter
+from template.dice.controller.dice_controller import diceResultRouter
+from template.include.socket_server.initializer.init_domain import DomainInitializer
+from template.system_initializer.init import SystemInitializer
+
+DomainInitializer.initEachDomain()
+SystemInitializer.initSystemDomain()
 
 app = FastAPI()
+
+load_dotenv()
+
+origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"],
+                   allow_headers=["*"], )
+
+app.include_router(deepLearningRouter)
+app.include_router(diceResultRouter)
+
+if __name__ == "__main__":
+    colorama.init()  # terminal상에서 가독성을 얻기 위한 출력 색상 라이브러리
+
+    TaskManager.createSocketServer()
+    uvicorn.run(app, host=os.getenv('HOST'), port=int(os.getenv('FASTAPI_PORT')))
 
 
 @app.get("/")
