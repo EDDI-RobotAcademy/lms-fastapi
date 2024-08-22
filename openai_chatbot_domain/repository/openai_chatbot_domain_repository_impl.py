4e8de67a08432caaaff04b9462be02cac3cb118f
Meue-L <meue7785@gmail.com>
2024-08-22 09:11:58 +0900

    feat: [MinJaeLee/Meue] OpenaiChatbot 기본 윤곽 형성 [LMSA-1]

---
+++ openai_chatbot_domain/repository/openai_chatbot_domain_repository_impl.py
@@ -0,0 +1,16 @@
+import json
+import queue
+
+from openai_chatbot_domain.repository.openai_chatbot_domain_repository import OpenaiChatbotDomainRepository
+
+
+class OpenaiChatbotDomainRepositoryImpl(OpenaiChatbotDomainRepository):
+    def getGeneratedRecipe(self, userDefinedReceiverFastAPIChannel):
+        print(f"OpenaiChatbotDomainRepositoryImpl getGeneratedRecipe()")
+
+        try:
+            receivedResponseFromSocketClient = userDefinedReceiverFastAPIChannel.get(False)
+            return json.loads(receivedResponseFromSocketClient)
+
+        except queue.Empty:
+            return "데이터 없음 저리 가"
