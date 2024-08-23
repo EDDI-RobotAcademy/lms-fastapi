5dc0fb4ef63f6fb19fbda12d19b9ea7778e9cc41
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-08-23 17:14:34 +0900

    feat: [최민지] openai chatbot tts 구현 [LMSA-4]

--- openai_chatbot_domain/service/openai_chatbot_domain_service.py
+++ openai_chatbot_domain/service/openai_chatbot_domain_service.py
@@ -1,6 +1,10 @@
-from abc import ABC
-
+from abc import ABC,abstractmethod
 
 class OpenaiChatbotDomainService(ABC):
+    @abstractmethod
     def getGeneratedRecipe(self):
+        pass
+
+    @abstractmethod
+    def getGeneratedVoice(self):
         pass
