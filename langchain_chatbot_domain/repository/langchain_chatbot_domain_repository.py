0f6864aa5f38599957ff6e7135985bb49236b5c0
Meue-L <meue7785@gmail.com>
2024-09-04 19:31:47 +0900

    feat: [MinJaeLee/Meue] langchain rag 기반 chatbot과 사용자의 요청을 주고 받을 수 있습니다 [LMSA-3]

--- langchain_chatbot_domain/repository/langchain_chatbot_domain_repository.py
+++ langchain_chatbot_domain/repository/langchain_chatbot_domain_repository.py
@@ -2,33 +2,9 @@ from abc import ABC, abstractmethod
 
 class LangchainChatbotDomainRepository(ABC):
     @abstractmethod
-    def loadDocumentation(self, filepath):
+    def getFaissIndex(self, userDefinedReceiverFastAPIChannel):
         pass
 
     @abstractmethod
-    def generatePrompt(self):
-        pass
-
-    @abstractmethod
-    def loadEmbeddingModel(self):
-        pass
-
-    @abstractmethod
-    def createFaissIndex(self, texts, embedding):
-        pass
-
-    @abstractmethod
-    def loadFaissIndex(self, faissIndexPath, embeddings):
-        pass
-
-    @abstractmethod
-    def loadLLMChain(self):
-        pass
-
-    @abstractmethod
-    def createChain(self, llm, prompt, faissIndex):
-        pass
-
-    @abstractmethod
-    def invokeChain(self, chain, userSendMessage):
+    def getGeneratedRecipe(self, userDefinedReceiverFastAPIChannel):
         pass
