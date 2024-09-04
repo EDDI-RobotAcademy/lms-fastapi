09b500804dfd3fb0ab30a19df2c6a1f9289f9808
Meue-L <meue7785@gmail.com>
2024-09-04 19:44:22 +0900

    fix: [MinJaeLee/Meue] 의존성 주입 누락 해결 [LMSA-3]

--- langchain_chatbot_domain/controller/langchain_chatbot_domain_controller.py
+++ langchain_chatbot_domain/controller/langchain_chatbot_domain_controller.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends, status
 from starlette.responses import JSONResponse
 from template.include.socket_server.utility.color_print import ColorPrinter
 from langchain_chatbot_domain.service.langchain_chatbot_domain_service_impl import LangchainChatbotDomainServiceImpl
+from user_defined_queue.repository.user_defined_queue_repository_impl import UserDefinedQueueRepositoryImpl
 
 langchainChatbotDomainRouter = APIRouter()
 async def injectLangchainChatbotDomainService() -> LangchainChatbotDomainServiceImpl:
-    return LangchainChatbotDomainServiceImpl()
+    return LangchainChatbotDomainServiceImpl(UserDefinedQueueRepositoryImpl.getInstance())
 
 @langchainChatbotDomainRouter.post("/generate-faiss-index")
 async def requestToLangchainGenerateRecipe(langchainChatbotDomainService: LangchainChatbotDomainServiceImpl =
